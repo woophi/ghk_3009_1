8cb255bbb2de8621fec79b560431a9e7734ea1e6
Konstantin Mikheev <mik.kon6610@gmail.com>
2024-07-26 09:48:37 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,108 @@
+import { ButtonMobile } from '@alfalab/core-components/button/mobile';
+import { CDNIcon } from '@alfalab/core-components/cdn-icon';
+import { Typography } from '@alfalab/core-components/typography';
+import { useCallback, useState } from 'react';
+import current from './assets/current.png';
+import empty from './assets/empty.png';
+import lesscolors from './assets/lesscolors.png';
+import min from './assets/min.png';
+import noimgs from './assets/noimgs.png';
+import { LS, LSKeys } from './ls';
+import { appSt } from './style.css';
+import { ThxLayout } from './thx/ThxLayout';
+
+const vars = [
+  {
+    title: 'Не выбрано',
+    img: empty,
+  },
+  {
+    title: 'Текущий вариант',
+    img: current,
+  },
+  {
+    title: 'Приглушили цвета',
+    img: lesscolors,
+  },
+  {
+    title: 'Без картинок',
+    img: noimgs,
+  },
+  {
+    title: 'Минималистичный',
+    img: min,
+  },
+];
+
+export const App = () => {
+  const [selectedVariant, setSelectedVariant] = useState(vars[0]);
+  const [loading, setLoading] = useState(false);
+  const [err, setError] = useState('');
+  const [thxShow, setThx] = useState(LS.getItem(LSKeys.ShowThx, false));
+  const [openAllVars, setOpenAllVars] = useState(false);
+
+  const isEmpty = selectedVariant.title === vars[0].title;
+
+  const submit = useCallback(() => {
+    if (isEmpty) {
+      setError('Выберите вариант оформления главного экрана');
+      return;
+    }
+    setLoading(true);
+    setThx(true);
+    setLoading(false);
+    // sendDataToGA(choice).then(() => {
+    //   LS.setItem(LSKeys.ShowThx, true);
+    //   setThx(true);
+    //   setLoading(false);
+    // });
+  }, [selectedVariant, isEmpty]);
+
+  if (thxShow) {
+    return <ThxLayout />;
+  }
+
+  return (
+    <>
+      <div className={appSt.container}>
+        <Typography.TitleResponsive tag="h1" view="medium" font="system" weight="medium">
+          Выберите вариант оформления главного экрана
+        </Typography.TitleResponsive>
+      </div>
+      <img src={selectedVariant.img} className={appSt.imgBg({ isEmpty })} />
+      <div style={{ height: '170px' }} />
+      <div className={appSt.bottomBtn}>
+        {openAllVars ? (
+          vars.map(v => (
+            <div
+              className={appSt.selectItem}
+              key={v.title}
+              onClick={() => {
+                setError('');
+                setSelectedVariant(v);
+                setOpenAllVars(false);
+              }}
+            >
+              <Typography.Text tag="p" view="primary-medium" defaultMargins={false}>
+                {v.title}
+              </Typography.Text>
+              {v === selectedVariant ? <CDNIcon name="glyph_checkmark_m" /> : null}
+            </div>
+          ))
+        ) : (
+          <>
+            <div className={appSt.selectItem} onClick={loading ? undefined : () => setOpenAllVars(true)}>
+              <Typography.Text tag="p" view="primary-medium" defaultMargins={false}>
+                {selectedVariant.title}
+              </Typography.Text>
+              <CDNIcon name="glyph_chevron-down_m" />
+            </div>
+            <ButtonMobile loading={loading} onClick={submit} block view="primary" size="l" colors="inverted" hint={err}>
+              Выбрать
+            </ButtonMobile>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
